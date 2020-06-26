7b5b9a735aa248d883728c9c97a0e32a7c6348d5
Fl0Cri <florent@inetis.ch>
2020-06-26 20:39:23 +0200

    Auditor will need to guess instrument from sound

---
+++ docker/image-auditor/src/utilities/sounds.js
@@ -0,0 +1,11 @@
+/**
+ * Here are the definition of our musician types and their instrument sound.
+ */
+
+export const sounds = new Map([
+    ['ti-ta-ti', 'piano'],
+    ['pouet', 'trumpet'],
+    ['trulu', 'flute'],
+    ['gzi-gzi', 'violin'],
+    ['boum-boum', 'drum'],
+]);
